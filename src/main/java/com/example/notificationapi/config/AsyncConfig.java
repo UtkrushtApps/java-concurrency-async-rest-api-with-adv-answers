debc7d8e3f831e403a0c9ea9995d7c9c86455a00
Naman Muley <naman.g.muley@gmail.com>
2025-10-17 13:22:17 -0700

    Add src/main/java/com/example/notificationapi/config/AsyncConfig.java

---
+++ src/main/java/com/example/notificationapi/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.example.notificationapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "notificationExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(32);
+        executor.setQueueCapacity(100); // Can be tuned per use-case
+        executor.setThreadNamePrefix("Notify-Async-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(20);
+        executor.initialize();
+        return executor;
+    }
+}
